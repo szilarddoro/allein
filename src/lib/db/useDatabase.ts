924f8fc4b453818a3aec7184777ce2f9831296d1
Szilárd Dóró <doroszilard@gmail.com>
2025-09-24 20:19:38 +0200

    chore: remove unused useDatabase hook

--- src/lib/db/useDatabase.ts
+++
@@ -1,9 +0,0 @@
-import { useQuery } from '@tanstack/react-query'
-import { getDatabase } from './database'
-
-export function useDatabase() {
-  return useQuery({
-    queryKey: ['database'],
-    queryFn: getDatabase,
-  })
-}

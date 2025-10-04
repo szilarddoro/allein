305eeaac7d519e92cdf2533aa4ef5ef4275ffbc1
Szilárd Dóró <doroszilard@gmail.com>
2025-10-04 19:14:35 +0200

    fix: improve typography

--- src/pages/browser/BrowserPage.tsx
+++ src/pages/browser/BrowserPage.tsx
@@ -1,12 +1,11 @@
-import { H1, H2, P } from '@/components/ui/typography'
+import { H1, P } from '@/components/ui/typography'
 
 export function BrowserPage() {
   return (
     <div className="flex-1 overflow-hidden flex justify-center items-center select-none">
-      <div className="flex flex-col gap-2 max-w-lg mx-auto w-full text-center -mt-12">
-        <H1 className="sr-only">Home</H1>
-        <H2>Welcome to Allein</H2>
-        <P className="text-muted-foreground">
+      <div className="flex flex-col gap-1.5 max-w-lg mx-auto w-full text-center -mt-12">
+        <H1 className="text-3xl/tight mb-0">Welcome to Allein</H1>
+        <P className="text-muted-foreground my-0 leading-normal">
           Allein is a modern note-taking app that allows you to take notes,
           create documents, and more.
         </P>

0e579de53bc9d681583ff9e91b0e0a913243f326
Szilárd Dóró <doroszilard@gmail.com>
2025-11-10 20:12:03 +0100

    cleanup: remove unused header from applayout

--- src/components/layout/AppLayout.tsx
+++ src/components/layout/AppLayout.tsx
@@ -1,27 +1,15 @@
-import { Hotkey } from '@/components/Hotkey'
 import { BaseLayout } from '@/components/layout/BaseLayout'
+import { PageLayout } from '@/components/layout/PageLayout'
 import { SearchDialog } from '@/components/search/SearchDialog'
 import { Sidebar } from '@/components/sidebar/Sidebar'
-import { Button } from '@/components/ui/button'
-import { Link } from '@/components/ui/link'
+import { TauriDragRegion } from '@/components/TauriDragRegion'
 import {
   ResizableHandle,
   ResizablePanel,
   ResizablePanelGroup,
 } from '@/components/ui/resizable'
-import {
-  Tooltip,
-  TooltipContent,
-  TooltipTrigger,
-} from '@/components/ui/tooltip'
-import { useLocationHistory } from '@/lib/useLocationHistory'
-import { useWindowState } from '@/lib/useWindowState'
 import { useAIFeatures } from '@/lib/ai/useAIFeatures'
-import {
-  CURRENT_PLATFORM,
-  NEW_FILE_MENU_EVENT,
-  NEW_FOLDER_MENU_EVENT,
-} from '@/lib/constants'
+import { NEW_FILE_MENU_EVENT, NEW_FOLDER_MENU_EVENT } from '@/lib/constants'
 import { useCreateFile } from '@/lib/files/useCreateFile'
 import { useCreateFolder } from '@/lib/files/useCreateFolder'
 import { useCurrentFolderPath } from '@/lib/files/useCurrentFolderPath'
@@ -30,13 +18,6 @@ import { AppLayoutContextProps } from '@/lib/types'
 import { useToast } from '@/lib/useToast'
 import { cn } from '@/lib/utils'
 import { useOnboardingProgress } from '@/pages/onboarding/useOnboardingProgress'
-import {
-  ChevronLeft,
-  ChevronRight,
-  Cog,
-  PanelLeftCloseIcon,
-  PanelLeftOpenIcon,
-} from 'lucide-react'
 import { useCallback, useEffect, useRef, useState } from 'react'
 import {
   ImperativePanelGroupHandle,
@@ -44,8 +25,6 @@ import {
 } from 'react-resizable-panels'
 import { Outlet, useLocation, useNavigate } from 'react-router'
 import { useMediaQuery } from 'usehooks-ts'
-import { PageLayout } from '@/components/layout/PageLayout'
-import { TauriDragRegion } from '@/components/TauriDragRegion'
 
 export function AppLayout() {
   useAIFeatures()
@@ -53,14 +32,12 @@ export function AppLayout() {
   const [searchOpen, setSearchOpen] = useState(false)
   const { mutateAsync: createFile } = useCreateFile()
   const { mutateAsync: createFolder } = useCreateFolder()
-  const { isFullscreen } = useWindowState()
   const navigate = useNavigate()
   const { pathname } = useLocation()
   const [currentFolderPath] = useCurrentFolderPath()
   const { toast } = useToast()
   const { data: progress, status: progressStatus } = useOnboardingProgress()
   const { data: files, status: filesStatus } = useFilesAndFolders()
-  const { goBack, goForward, canGoBack, canGoForward } = useLocationHistory()
   const panelGroupRef = useRef<ImperativePanelGroupHandle | null>(null)
   const sidebarPanelRef = useRef<ImperativePanelHandle | null>(null)
   const isLargeScreen = useMediaQuery('(min-width: 1920px)')
@@ -226,81 +203,6 @@ export function AppLayout() {
 
   return (
     <BaseLayout>
-      <header
-        className={cn(
-          'hidden fixed top-0 left-0 w-full pl-4 pr-2 py-2 justify-between items-center gap-0.5 z-10',
-          CURRENT_PLATFORM === 'macos' ? (isFullscreen ? 'pl-2' : 'pl-22') : '',
-        )}
-      >
-        <div className="flex items-center gap-2 relative z-20">
-          {files.length > 0 && (
-            <Tooltip delayDuration={500}>
-              <TooltipTrigger asChild>
-                <Button
-                  type="button"
-                  variant="ghost"
-                  size="icon"
-                  onClick={() => setSidebarOpen(!sidebarOpen)}
-                >
-                  {sidebarOpen ? (
-                    <PanelLeftCloseIcon aria-hidden="true" />
-                  ) : (
-                    <PanelLeftOpenIcon aria-hidden="true" />
-                  )}
-                </Button>
-              </TooltipTrigger>
-
-              <TooltipContent
-                align={
-                  CURRENT_PLATFORM === 'macos' && isFullscreen
-                    ? 'start'
-                    : 'center'
-                }
-                side="bottom"
-              >
-                {sidebarOpen ? 'Close Sidebar' : 'Open Sidebar'}
-              </TooltipContent>
-            </Tooltip>
-          )}
-
-          <Button
-            variant="ghost"
-            size="icon"
-            onClick={goBack}
-            disabled={!canGoBack}
-            aria-label="Go Back"
-          >
-            <ChevronLeft className="size-4.5" />
-          </Button>
-
-          <Button
-            variant="ghost"
-            size="icon"
-            onClick={goForward}
-            disabled={!canGoForward}
-            aria-label="Go Forward"
-          >
-            <ChevronRight className="size-4.5" />
-          </Button>
-        </div>
-
-        <div className="z-20">
-          <Tooltip delayDuration={500}>
-            <TooltipTrigger asChild>
-              <Button variant="ghost" size="icon" asChild>
-                <Link to="/settings" className="cursor-default" viewTransition>
-                  <Cog className="size-4" />
-                </Link>
-              </Button>
-            </TooltipTrigger>
-
-            <TooltipContent align="end" side="bottom">
-              Open Preferences <Hotkey modifiers={['meta']} keyCode="," />
-            </TooltipContent>
-          </Tooltip>
-        </div>
-      </header>
-
       <SearchDialog open={searchOpen} onOpenChange={setSearchOpen} />
 
       <main className="relative flex-auto overflow-hidden flex z-0">

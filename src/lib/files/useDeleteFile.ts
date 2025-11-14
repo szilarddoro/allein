9e494f1a5191248aa7623cc0a980ec8238aad00a
Szilárd Dóró <doroszilard@gmail.com>
2025-11-14 14:27:54 +0100

    fix: prevent missing file error on delete

--- src/lib/files/useDeleteFile.ts
+++ src/lib/files/useDeleteFile.ts
@@ -1,8 +1,7 @@
+import { useLogger } from '@/lib/logging/useLogger'
 import { useMutation, useQueryClient } from '@tanstack/react-query'
 import { invoke } from '@tauri-apps/api/core'
 import { FILES_AND_FOLDERS_TREE_QUERY_KEY } from './useFilesAndFolders'
-import { useLogger } from '@/lib/logging/useLogger'
-import { READ_FILE_QUERY_KEY } from '@/lib/files/useReadFile'
 
 export function useDeleteFile() {
   const queryClient = useQueryClient()
@@ -10,12 +9,9 @@ export function useDeleteFile() {
 
   return useMutation({
     mutationFn: (filePath: string) => invoke('delete_file', { filePath }),
-    onSuccess: async (_data, filePath) => {
+    onSuccess: async () => {
       try {
         await Promise.all([
-          queryClient.invalidateQueries({
-            queryKey: READ_FILE_QUERY_KEY(filePath),
-          }),
           queryClient.invalidateQueries({
             queryKey: FILES_AND_FOLDERS_TREE_QUERY_KEY(),
           }),
